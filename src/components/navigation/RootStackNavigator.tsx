4738f3cce1dd53e7227b689b1abe2eb09ae2f6c5
junho.nam <nowonlyhere@gmail.com>
2019-01-02 12:46:05 +0900

    first commit

---
+++ src/components/navigation/RootStackNavigator.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { observer } from 'mobx-react/native';
+
+import { colors } from '../../utils/Styles';
+import appStore from '../../stores/appStore';
+import IntroScreen from '../screen/Intro';
+import NotFoundScreen from '../screen/NotFound';
+
+const routeConfig = {
+  Intro: {
+    screen: IntroScreen,
+    navigationOptions: {
+      title: 'Intro',
+    },
+    path: 'intro',
+  },
+  NotFound: {
+    screen: NotFoundScreen,
+    path: 'NotFound',
+  },
+};
+
+const navigatorConfig = {
+  initialRouteName: 'Intro',
+  // header: null,
+  // headerMode: 'none',
+  gesturesEnabled: true,
+  statusBarStyle: 'light-content',
+  navigationOptions: {
+    headerStyle: {
+      headerBackTitle: null,
+      backgroundColor: colors.dodgerBlue,
+      borderBottomColor: 'transparent',
+      borderBottomWidth: 0,
+      elevation: 0,
+    },
+    headerTitleStyle: { color: 'white' },
+    headerTintColor: 'white',
+  },
+};
+
+const RootStackNavigator = createStackNavigator(routeConfig, navigatorConfig);
+
+interface IProps {
+  navigation: any;
+}
+
+@observer
+class RootNavigator extends React.Component<IProps> {
+  private static router = RootStackNavigator.router;
+
+  public render() {
+    return <RootStackNavigator navigation={this.props.navigation}/>;
+  }
+}
+
+export default RootNavigator;
---
+++ src/components/screen/Intro.tsx
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  FlatList,
+  InteractionManager,
+} from 'react-native';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { inject } from 'mobx-react/native';
+
+import { ratio, colors } from '../../utils/Styles';
+import { IC_MASK } from '../../utils/Icons';
+import User from '../../models/User';
+import { getString } from '../../../STRINGS';
+import Button from '../shared/Button';
+
+const styles: any = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  titleTxt: {
+    marginTop: 100,
+    color: colors.dusk,
+    fontSize: 24,
+  },
+  txtLogin: {
+    fontSize: 14,
+    color: 'white',
+  },
+  imgBtn: {
+    width: 24,
+    height: 24,
+    position: 'absolute',
+    left: 16,
+  },
+  viewUser: {
+    marginTop: 60,
+    alignItems: 'center',
+  },
+  txtUser: {
+    fontSize: 16,
+    color: colors.dusk,
+    lineHeight: 48,
+  },
+  btnBottomWrapper: {
+    position: 'absolute',
+    bottom: 40,
+  },
+  btnLogin: {
+    backgroundColor: colors.dodgerBlue,
+    alignSelf: 'center',
+    borderRadius: 4,
+    width: 320,
+    height: 52,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnNavigate: {
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    borderRadius: 4,
+    width: 320,
+    height: 52,
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+interface IProps {
+  navigation?: any;
+  store: any;
+}
+
+interface IState {
+  isLoggingIn: boolean;
+}
+
+@inject('store') @observer
+class Page extends Component<IProps, IState> {
+  private timer: any;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggingIn: false,
+    };
+  }
+
+  public componentWillUnmount() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+  }
+
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleTxt}>paycoWebErrorSample</Text>
+        <View style={styles.viewUser}>
+          <Text style={styles.txtUser}>{this.props.store.user.displayName}</Text>
+          <Text style={styles.txtUser}>{this.props.store.user.age}</Text>
+          <Text style={styles.txtUser}>{this.props.store.user.job}</Text>
+        </View>
+        <View style={styles.btnBottomWrapper}>
+          <Button
+            isLoading={this.state.isLoggingIn}
+            onPress={this.onLogin}
+            style={styles.btnLogin}
+            textStyle={styles.txtLogin}
+            imgLeftSrc={IC_MASK}
+            imgLeftStyle={styles.imgBtn}
+          >{getString('LOGIN')}</Button>
+          <Button
+            onPress={() => this.props.navigation.navigate('NotFound') }
+            style={[
+              styles.btnNavigate,
+              {
+                marginTop: 15,
+              },
+            ]}
+            textStyle={{
+              color: colors.dodgerBlue,
+            }}
+          >Navigate</Button>
+        </View>
+      </View>
+    );
+  }
+
+  private onLogin = () => {
+    this.props.store.user = new User();
+    this.setState({ isLoggingIn: true }, () => {
+      this.timer = setTimeout(() => {
+        this.props.store.user.displayName = 'dooboolab';
+        this.props.store.user.age = 30;
+        this.props.store.user.job = 'developer';
+        this.setState({ isLoggingIn: false });
+      }, 1000);
+    });
+  }
+}
+
+export default Page;
